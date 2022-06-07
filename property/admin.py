18e467bcf51c6f6088f8e64c7e33f7b705021b03
redbor24 <redbor24@gmail.com>
2022-06-07 17:25:45 +0300

    Add model field Flat.new_building.

--- property/admin.py
+++ property/admin.py
@@ -5,8 +5,8 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
-    list_display = ('town', 'town_district', 'address', 'floor', 'rooms_number', 'living_area', 'active',
-                    'construction_year', 'price')
+    list_display = ('town', 'town_district', 'new_building', 'address', 'floor', 'rooms_number', 'living_area',
+                    'active', 'construction_year', 'price')
     readonly_fields = ['created_at']
 
 
