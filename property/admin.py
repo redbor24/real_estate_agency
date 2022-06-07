00ec8eeee4791dec40f007278b2983fa22f48d33
redbor24 <redbor24@gmail.com>
2022-06-07 17:50:10 +0300

    setup Flat changelist page.

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'town_district', 'new_building', 'address', 'floor', 'rooms_number', 'living_area',
                     'active', 'construction_year', 'price')
     readonly_fields = ['created_at']
+    list_editable = ['new_building']
+    list_per_page = 10
 
 
 admin.site.register(Flat, FlatAdmin)
