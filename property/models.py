7e5a29aaa2e155a75c4bb02d6592b5978c191d65
redbor24 <redbor24@gmail.com>
2022-06-22 13:33:47 +0300

    Добавлено related_name='likes' полю Flat.like

--- property/models.py
+++ property/models.py
@@ -47,7 +47,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('Новостройка', null=True, db_index=True)
-    like = models.ManyToManyField(User, verbose_name='Кто лайкнул')
+    like = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='likes')
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
